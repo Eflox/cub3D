8684db7030e6d8f8cd8889d317689a986b60249f
Charles Dansembourg <charlesdansembourg@charless-mbp.home>
2021-02-28 12:12:29 +0100

    v1

---
+++ include/cub3D.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3D.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: charlesdansembourg <marvin@42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/28 11:13:45 by charlesda         #+#    #+#             */
+/*   Updated: 2021/02/28 11:16:59 by charlesda        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef _CUB3D_H_
+# define _CUB3D_H_
+
+# include  "../mlx/mlx.h"
+
+# include <stdlib.h>
+# include <string.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include <math.h>
+# include <fcntl.h>
+
+#endif
